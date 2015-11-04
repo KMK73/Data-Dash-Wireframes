80b7de1d62483d5a2c68361eeecc5c8c79d5210d
KMK73 <kmk73@miami.edu>
2015-11-04 00:09:31 -0500

    dashboard update based on filtered wireframe

---
+++ 311-dashboard/js/plugins/morris/morris-line-overdue-tickets.js
@@ -0,0 +1,48 @@
+// Line Chart
+    Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'morris-line-chart-overdue',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: [{
+            d: '2012-10-01',
+            visits: 802
+        }, {
+            d: '2012-10-02',
+            visits: 783
+        }, {
+            d: '2012-10-03',
+            visits: 820
+        }, {
+            d: '2012-10-04',
+            visits: 839
+        }, {
+            d: '2012-10-05',
+            visits: 792
+        }, {
+            d: '2012-10-06',
+            visits: 859
+        }, {
+            d: '2012-10-07',
+            visits: 790
+        }, {
+            d: '2012-10-08',
+            visits: 1680
+        }, {
+            d: '2012-10-09',
+            visits: 1592
+        }, {
+            d: '2012-10-10',
+            visits: 1420
+        }],
+        // The name of the data record attribute that contains x-visitss.
+        xkey: 'd',
+        // A list of names of data record attributes that contain y-visitss.
+        ykeys: ['visits'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Visits'],
+        // Disables line smoothing
+        smooth: false,
+        resize: true
+    });
